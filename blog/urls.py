33f987e3bfbd8d099edd6d40658f17decf14c082
erykslezak <erykslezak@outlook.ie>
2022-05-03 20:48:22 +0000

    Link edit profile page in nav

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import AddPost, PostDetail, EditPost, DeletePost, UserPostList, Tags
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('user_posts/<str:username>', views.UserPostList.as_view(), name='user_posts'),
-    path('account/edit_profile', EditProfileView.as_view(), name="edit_profile"),
+    path('account/edit_profile/<str:username>', EditProfileView.as_view(), name="edit_profile"),
     path('add_post/', AddPost.as_view(), name="add_post"),
     path('edit_post/<slug:slug>', EditPost.as_view(), name="edit_post"),
     path('delete_post/<slug:slug>', DeletePost.as_view(), name="delete_post"),
